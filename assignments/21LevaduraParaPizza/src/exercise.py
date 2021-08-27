509095ed2238fd4f1768aee8f945b8967c7c583c
LuisAzuara <89654931+LuisAzuara@users.noreply.github.com>
2021-08-27 16:35:47 -0500

    archivo pizza finalizado

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,5 @@
-def main():
-    # escribe tu código abajo de esta línea
-    pass
+harina = float(input("Dame la harina en gramos: "))
 
+levadura = harina * 0.050
 
-if __name__ == '__main__':
-    main()
+print("Necesitas estos gramos de levadura:",levadura)
