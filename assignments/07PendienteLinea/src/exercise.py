cd59e75116db958a76d0ac66f4f0f795bdcc899b
LuisAzuara <89654931+LuisAzuara@users.noreply.github.com>
2021-08-27 16:27:04 -0500

    archivo resuelto de la pendiente

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,6 @@
-def main():
-    # escribe tu código abajo de esta línea
-    pass
-
-
-if __name__ == '__main__':
-    main()
+x1 = float(input("Dame x1: "))
+y1 = float(input("Dame y1: "))
+x2 = float(input("Dame x2: "))
+y2 = float(input("Dame y2: "))
+n = (y2 - y1) / (x2 - x1)
+print("Pendiente:", n)
