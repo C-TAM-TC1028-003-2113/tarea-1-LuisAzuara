95ac0279dade5507d00951c2130e85581173bd3f
LuisAzuara <89654931+LuisAzuara@users.noreply.github.com>
2021-08-31 12:14:41 -0500

    Add files via upload
    listo

---
+++ assignments/09Telefono/src/exersice.py
@@ -0,0 +1,10 @@
+def main()
+
+mensajes = float(input("Dame el número de mensajes: "))
+megas = float(input("Dame el número de megas: "))
+minutos = float(input("Dame el número de minutos: "))
+costo = (mensajes * 0.80)+(megas * 0.80)+(minutos * 0.80)
+print("El costo mensual es:",costo)
+
+if__name__""'__main__':
+  main()
