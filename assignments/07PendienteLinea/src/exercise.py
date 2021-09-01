387ce9d637c88f4c529618ad993679953b45a572
LuisAzuara <89654931+LuisAzuara@users.noreply.github.com>
2021-08-31 21:47:24 -0500

    Add files via upload
    archivo correcto

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,10 @@
-x1 = float(input("Dame x1: "))
-y1 = float(input("Dame y1: "))
-x2 = float(input("Dame x2: "))
-y2 = float(input("Dame y2: "))
-n = (y2 - y1) / (x2 - x1)
-print("Pendiente:", n)
+def main():
+   x1 = float(input("Dame x1: "))
+   y1 = float(input("Dame y1: "))
+   x2 = float(input("Dame x2: "))
+   y2 = float(input("Dame y2: "))
+   n = (y2 - y1) / (x2 - x1)
+   print("Pendiente:", n)
+
+if __name__ == '__main__':
+    main()
